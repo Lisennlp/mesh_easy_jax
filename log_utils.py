4e34f814e86838aa6b28da483fbb63b6f8da3e3f
lishengping2010@163.com <lishengping2010@163.com>
2023-07-12 18:01:03 +0800

    logger file to bucket

--- log_utils.py
+++ log_utils.py
@@ -3,7 +3,10 @@ import logging
 import os
 import jax
 import subprocess
+import smart_open
 
+
+# 重写logger的log函数
 class CustomLogger(logging.Logger):
     def _log(self, level, msg, args, exc_info=None, extra=None, stack_info=False, stacklevel=1):
         if extra is None:
@@ -11,13 +14,27 @@ class CustomLogger(logging.Logger):
         extra['host_id'] = jax.process_index()
         super()._log(level, msg, args, exc_info, extra, stack_info, stacklevel)
 
+# 重写FileHander的open函数
+class CustomHandler(logging.FileHandler):
+    def _open(self):
+        if 'gs:' in self.baseFilename:
+            filename = self.baseFilename.split('gs:')[-1]
+            # filename: /jax_llm_logs/....
+            return smart_open.open(f'gs:/{filename}', 'w')
+        else:
+            return open(self.baseFilename, self.mode, encoding=self.encoding)
+
 def setup_logger(host_id):
     logger = CustomLogger('my_logger')
     logger.setLevel(logging.DEBUG)
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    log_filename = f"logs/log_{current_time}_{host_id}.txt"
+    # 只把主节点的日志写入到bucket
+    if host_id == 0:
+        log_filename = f"gs://jax_llm_logs/train_log_{current_time}_{host_id}.txt"
+    else:
+        log_filename = f"log_{current_time}_{host_id}.txt"
     file_handler = logging.FileHandler(log_filename)
     file_handler.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
@@ -27,16 +44,6 @@ def setup_logger(host_id):
     console_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-    # 注册一个清理函数，在关闭日志处理器时上传日志文件
-    def cleanup():
-        file_handler.close()
-        command = f'gsutil cp {log_filename} gs://jax_llm_logs/'
-        response = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
-    # 在 Python 解释器关闭时自动执行清理函数
-    if host_id == 0:
-        import atexit
-        atexit.register(cleanup)
     return logger
 
-# 使用示例
 logger = setup_logger(jax.process_index())
