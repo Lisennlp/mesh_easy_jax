1d5970284c2b1628e156e1e7728a87abf1ff61d3
lishengping2010@163.com <lishengping2010@163.com>
2024-01-18 14:28:04 +0800

    debug

--- easylm/llama_model.py
+++ easylm/llama_model.py
@@ -1583,7 +1583,7 @@ class FlaxLLaMAForCausalLMModule(BaseLayer):
 class FlaxLLaMAForCausalLM(FlaxLLaMAPreTrainedModel):
     module_class = FlaxLLaMAForCausalLMModule
 
-    def prepare_inputs_for_generation(self, input_ids, max_length, attention_mask: Optional[jnp.DeviceArray] = None):
+    def prepare_inputs_for_generation(self, input_ids, max_length, attention_mask = None):
         # initializing the cache
         batch_size, seq_length = input_ids.shape
 
